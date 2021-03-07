eb1fb7e0d502dad0d18cb0c9326abe41ad996696
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-03-07 18:07:59 -0300

    Added local DB config

--- src/mercado/Banco/Banco.java
+++ src/mercado/Banco/Banco.java
@@ -6,6 +6,7 @@ public class Banco
     static private String url = "jdbc:postgresql://isilo.db.elephantsql.com:5432/yjyujddp";
     static private String username = "yjyujddp" ;
     static private String password = "5K59TBI1sCXdE7zRewa3Xxy60Cv3LRAN";
+    static private Boolean prod = false;
            
     
     static public Conexao getCon()
@@ -18,6 +19,10 @@ public class Banco
     static public boolean conectar()
     {
         con = new Conexao(); 
-        return con.conectar(url, username, password);
+        
+        if(prod)
+            return con.conectar(url, username, password);
+        else
+            return con.conectar("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
     }
 }
